41bf831708d9d89ae1e9d85c67f69c12c1388ddb
edgz98 <edgz98@hotmail.com>
2024-10-03 18:37:11 +0900

    Refactoring of InputArea Code, readability

--- src/components/InputArea.jsx
+++ src/components/InputArea.jsx
@@ -3,32 +3,26 @@ import React, { useState } from "react";
 const InputArea = (props) => {
   const [userInput, setUserInput] = useState("");
 
-  const handleInputChange = (event) => {
-    const newValue = event.target.value;
-    setUserInput(newValue);
-  };
-
   return (
     <div>
-      <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (userInput.trim().length > 0) {
+            props.onAdd(userInput);
+            setUserInput("");
+          } else {
+          }
+        }}
+      >
         <input
-          onChange={handleInputChange}
+          onChange={(e) => setUserInput(e.target.value)}
           type="text"
           placeholder="Next task..."
           value={userInput}
         />
-        <button
-          onClick={() => {
-            if (userInput.trim().length > 0) {
-              props.onAdd(userInput);
-              setUserInput("");
-            } else {
-            }
-          }}
-        >
-          Add
-        </button>
-      </div>
+        <button type="submit">Add</button>
+      </form>
     </div>
   );
 };
